91b01e115328c68a93b396a1bf9ef616a5bdee33
Davys Lima <davysjunior08@hotmail.com>
2021-03-16 00:41:48 -0300

    Feature: Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
-export function Header() {
+
+interface HeaderProps {
+  onOpenNewTransactionsModal: () => void;
+}
+export function Header({ onOpenNewTransactionsModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button>Nova transacao</button>
+        <button type="button" onClick={onOpenNewTransactionsModal}>
+          Nova transacao
+        </button>
       </Content>
     </Container>
   );
