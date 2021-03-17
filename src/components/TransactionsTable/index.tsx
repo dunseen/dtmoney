daf58e3ffe331bb4e666be94db53ebf19990a24e
Davys Lima <davysjunior08@hotmail.com>
2021-03-16 23:14:11 -0300

    Feature: Formatiing amount

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -34,9 +34,19 @@ export function TransactionsTable() {
           {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.value}</td>
+              <td className={transaction.type}>
+                {transaction.type === "withdraw" && "-"}
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.value)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.value)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
